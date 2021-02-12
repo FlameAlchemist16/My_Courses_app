a6d952c8c0b62c6278cf57e08cee40ce0d9bed09
Mridul Khare <54219082+FlameAlchemist16@users.noreply.github.com>
2021-02-12 20:04:03 +0530

    Add files via upload

--- app/src/main/java/com/example/android/course_schedule_tracker/CourseDetail.java
+++ app/src/main/java/com/example/android/course_schedule_tracker/CourseDetail.java
@@ -9,5 +9,13 @@ public class CourseDetail extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_course_detail);
+
+//        content
+//        array -->
+        String [] aboutCourse1 = new String[3];
+        aboutCourse1[0]="ESC201";
+        aboutCourse1[1]="credits => 9";
+        aboutCourse1[2]="Tuesday, Thursday";
+
     }
 }
