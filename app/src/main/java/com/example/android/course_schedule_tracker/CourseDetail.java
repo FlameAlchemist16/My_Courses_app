81342800a1259a77c89a2a5e957b911cff14688e
Mridul Khare <54219082+FlameAlchemist16@users.noreply.github.com>
2021-02-17 23:35:55 +0530

    Add files via upload

--- app/src/main/java/com/example/android/course_schedule_tracker/CourseDetail.java
+++ app/src/main/java/com/example/android/course_schedule_tracker/CourseDetail.java
@@ -2,6 +2,10 @@ package com.example.android.course_schedule_tracker;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import java.util.ArrayList;
 
 public class CourseDetail extends AppCompatActivity {
 
@@ -16,6 +20,19 @@ public class CourseDetail extends AppCompatActivity {
         aboutCourse1[0]="ESC201";
         aboutCourse1[1]="credits => 9";
         aboutCourse1[2]="Tuesday, Thursday";
-
+//        array list -->
+        ArrayList<String> aboutCourse11 = new ArrayList<>();
+        aboutCourse11.add("ESC201");
+        aboutCourse11.add("9 credits");
+        aboutCourse11.add("Tuesday");
+        int a=0;
+        LinearLayout cView = (LinearLayout) findViewById(R.id.c01);
+        while(a<aboutCourse11.size()){
+            TextView cView1 = new TextView(this);
+            cView1.setText(aboutCourse11.get(a));
+            cView.addView(cView1);
+            a++;
+//            cView.removeView(cView1);
+        }
     }
 }
